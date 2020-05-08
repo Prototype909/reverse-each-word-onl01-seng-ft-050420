c40a4d22d3996b33c7cefcfdd7e17fdc4d9d3398
Steve Anderson <prototype909@msn.com>
2020-05-08 01:53:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
 
 
 def reverse_each_word(string)
- array = string.split(" ")
- test_array = []
- array.collect do|string|
-   test_array << string.reverse
- end
- test_array.join(" ")
+  split_string = string.split(" ")
+  new_array = []
+  new_array = split_string.collect {|word| word.reverse}
+  new_array.join(" ")
 end
